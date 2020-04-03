7cb3804fbde60ffed359332d6a8804132d64baee
valtercioj <53585971+valtercioj@users.noreply.github.com>
2020-04-02 22:26:37 -0300

    Update 1010.py

--- 1010.py
+++ 1010.py
@@ -1,16 +1,15 @@
-v1 = input().split(" ")
-v2 = input().split(" ")
+cod1,qtd1,val1 = map(str,input().split())
 
-cod1 = int(v1[0])
-qtd1 = int(v1[1])
-val1 = float(v1[2])
+cod1 = int(cod1)
+qtd1= int(qtd1)
+val1 = float(val1)
 tot1 = qtd1 * val1
+cod2,qtd2,val2 = map(str,input().split())
 
-cod2 = int(v2[0])
-qtd2 = int(v2[1])
-val2 = float(v2[2])
+cod2 = int(cod2)
+qtd2= int(qtd2)
+val2 = float(val2)
 tot2 = qtd2 * val2
+valor = tot1 + tot2
 
-tot = tot1 + tot2
-
-print("VALOR A PAGAR: R$ {0:.2f}".format(tot))
+print('VALOR A PAGAR: R$ {:.2f}'.format(valor))
